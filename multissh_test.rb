4b1fac35742d5ee65d60b6588cc46ecf4c2ae9e7
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-05-28 15:39:25 -0600

    The program now is able to ssh into nodes
    still unable to run commands on the nodes

--- multissh_test.rb
+++ multissh_test.rb
@@ -9,12 +9,12 @@ print "\n#{op_sys}\n"
 print "\nWhat is the name of your private key associated with your emulab account?\n"
 priv_key_name = gets.to_s.delete("\n")
 print "\nEnter the username to connect to host\n"
-username = gets.to_s.delete("\n")
+user_name = gets.to_s.delete("\n")
 print "\nHow many nodes are you trying to connect to?\n"
 # need to cast to int?
 num_hosts = gets.to_i
-print "\nWhat is your password?\n"
-password = gets.to_s.delete("\n")
+#print "\nWhat is your password?\n"
+#password = gets.to_s.delete("\n")
 hosts = Array.new(num_hosts)
 ports = Array.new(num_hosts)
 commands = Array.new(num_hosts)
@@ -22,27 +22,33 @@ commands = Array.new(num_hosts)
 for i in 0..num_hosts-1
     puts "\nWhat is the Host name for node #{i+1}? Example: pc10.emulab.net\n"
     cur_host = gets.to_s
-    hosts[i] = cur_host
+    hosts[i] = cur_host.to_s.delete("\n")
     puts "\nWhat is the port number for this host?\n"
-    ports[i] = gets.to_s
-    command = "#{username}@#{hosts[i]}"
-    #if op_sys == 'LINUX'
-        #command = "ssh -X -p #{port_no} #{username}@#{hosts[i]}"
-    #elsif op_sys == 'WINDOWS'
-        #command = "ssh -i ~/.ssh/#{priv_key_name} -p #{port_no} #{username}@#{hosts[i]}"
-    #end
+    ports[i] = gets.to_s.delete("\n")
+    #command = "#{username}@#{hosts[i]}"
+    command = ""
+    if op_sys == 'LINUX'
+        command = "ssh -X -p #{ports[i]} #{user_name}@#{hosts[i]}"
+    elsif op_sys == 'WINDOWS'
+        command = "ssh -i ~/.ssh/#{priv_key_name} -p #{ports[i]} #{user_name}@#{hosts[i]}"
+    end
     commands[i] = command.delete("\n")
+    exec(commands[i].to_s)
     print "\nCurrent command is:\n" + commands[i] + " \n" 
 end
+#****** [THIS SORTA WORKS, CANT GET IT TO RUN THE COMMAND WHEN SSH'ED INTO THE NODE] ******
+# Net::SSH.start(hosts[0],user_name) do |session|
+#     session.exec 'sudo -su'
+# end
+# ****** [THIS SORTA WORKS BUT I DONT THINK IT IS WHAT I AM LOOKING FOR] ******
 # Connecting to the hosts and start executing commands
-Net::SSH.start( hosts[0].to_s, :password=>password.to_s, :port=>ports[0].to_s, :username=>username.to_s, :host_key=>priv_key_name.to_s) do |session|
-    some_func(session, "ls")
-end
-
-def some_func(session, command)
-    session.open_channel do |channel|
-        channel.on_data do |ch, data|
-        end
-    channel.exec command.to_s
+Net::SSH::Multi.start do |session|
+    # Connect to remote machines
+    ### Change this!!###
+    session.use "#{username}@#{hosts[0]}"
+    session.exec('sudo -su') do |ch, stream, data|
+      puts "[#{ch[:host]} : #{stream}] #{data}"
     end
+    # Tell Net::SSH to wait for output from the SSH server
+    session.loop
 end
