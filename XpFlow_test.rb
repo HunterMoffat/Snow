0c5369398864d3cb054abc00076fc1307e1416d9
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-06-04 12:44:31 -0600

    some test scripts for XpFlow

---
+++ XpFlow_test.rb
@@ -0,0 +1,28 @@
+# A simple XpFlow Test 
+require 'net/ssh'
+require 'net/ssh/multi'
+
+
+h1 = 'pc06-fortvm-2.emulab.net'
+h2 = 'pc06-fortvm-1.emulab.net'
+u = 'Hmoffat'
+
+process :main do
+  setup_ssh
+end
+dir = "/ems/"
+# This will ssh into each node and will clone my ems repo to it so It can run my scripts
+
+activity :setup_ssh do
+  Net::SSH.start(h1, u) do |session|
+    session.exec!('git clone https://github.com/HunterMoffat/ems.git')
+    session.exec!("cd #{dir}")
+    session.exec!('ruby sim_simple.rb')
+  end
+
+#   Net::SSH.start(h2, u) do |session|
+#     session.exec!('git clone https://github.com/HunterMoffat/ems.git')
+#     session.exec!("cd #{dir}")
+#     session.exec!('ruby epc_simple.rb')
+#   end
+end
