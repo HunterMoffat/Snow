eaa93ffe77c640160eedce29e991194aa095dec3
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-06-05 08:57:48 -0600

    Fixed some minor bugs

--- multissh_test.rb
+++ multissh_test.rb
@@ -9,14 +9,17 @@ dir = "/ems/"
 # This will ssh into each node and will clone my ems repo to it so It can run my scripts
 
 Net::SSH.start(h1, u) do |session|
-    session.exec!('git clone https://github.com/HunterMoffat/ems.git')
-    session.exec!("cd #{dir}")
-    session.exec!('ruby sim_simple.rb')
+  @result = session.exec!('git clone https://github.com/HunterMoffat/ems.git')
+  puts @result
+  @result = session.exec!('cd ems
+    ruby sim_simple.rb')
+  puts @result
 end
 
-#   Net::SSH.start(h2, u) do |session|
-#     session.exec!('git clone https://github.com/HunterMoffat/ems.git')
-#     session.exec!("cd #{dir}")
-#     session.exec!('ruby epc_simple.rb')
-#   end
+puts 'Now ssh-ing into epc'
 
+Net::SSH.start(h2, u) do |session|
+  session.exec!('git clone https://github.com/HunterMoffat/ems.git')
+  session.exec!('cd #ems')
+  session.exec!('ruby epc.rb')
+end
