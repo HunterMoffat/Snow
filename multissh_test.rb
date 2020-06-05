8077f77325f44bf6ea386fa324cf1327a731f998
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-06-05 12:54:31 -0600

    added multi threading to the script

--- multissh_test.rb
+++ multissh_test.rb
@@ -2,26 +2,31 @@
 require 'net/ssh'
 require 'net/ssh/multi'
 
-h1 = 'pc03-mebvm-2.emulab.net'
-h2 = 'pc03-mebvm-1.emulab.net'
+h1 = 'pc14-fortvm-2.emulab.net'
+h2 = 'pc14-fortvm-1.emulab.net'
 u = 'Hmoffat'
 dir = "/ems/"
 # This will ssh into each node and will clone my ems repo to it so It can run my scripts
 puts 'Now ssh-ing into epc'
-
-Net::SSH.start(h2, u) do |session|
-  @result = session.exec!('git clone https://github.com/HunterMoffat/ems.git')
-  puts @result
-  session.exec!('cd ems')
-  @result = puts @result
-  session.exec!('ruby epc.rb')
-  @result = puts @result
+sim = Thread.new do 
+  Net::SSH.start(h2, u) do |session|
+    @result = session.exec!('git clone https://github.com/HunterMoffat/ems.git')
+    puts @result
+    session.exec!('cd ems')
+    @result = puts @result
+    session.exec!('ruby epc.rb')
+    @result = puts @result
+  end
 end
+sim.join
 
-Net::SSH.start(h1, u) do |session|
-  @result = session.exec!('git clone https://github.com/HunterMoffat/ems.git')
-  puts @result
-  @result = session.exec!('cd ems
-    ruby sim_simple.rb')
-  puts @result
+epc = Thread.new do
+  Net::SSH.start(h1, u) do |session|
+    @result = session.exec!('git clone https://github.com/HunterMoffat/ems.git')
+    puts @result
+    @result = session.exec!('cd ems
+      ruby sim_simple.rb')
+    puts @result
+  end
 end
+epc.join
