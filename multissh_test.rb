b3e505fd9b695056cd3e2b6776aeedca5f96306e
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-06-05 12:36:19 -0600

    removed a command that was causing errors

--- multissh_test.rb
+++ multissh_test.rb
@@ -2,11 +2,21 @@
 require 'net/ssh'
 require 'net/ssh/multi'
 
-h1 = 'pc06-fortvm-2.emulab.net'
-h2 = 'pc06-fortvm-1.emulab.net'
+h1 = 'pc03-mebvm-2.emulab.net'
+h2 = 'pc03-mebvm-1.emulab.net'
 u = 'Hmoffat'
 dir = "/ems/"
 # This will ssh into each node and will clone my ems repo to it so It can run my scripts
+puts 'Now ssh-ing into epc'
+
+Net::SSH.start(h2, u) do |session|
+  @result = session.exec!('git clone https://github.com/HunterMoffat/ems.git')
+  puts @result
+  session.exec!('cd ems')
+  @result = puts @result
+  session.exec!('ruby epc.rb')
+  @result = puts @result
+end
 
 Net::SSH.start(h1, u) do |session|
   @result = session.exec!('git clone https://github.com/HunterMoffat/ems.git')
@@ -14,12 +24,4 @@ Net::SSH.start(h1, u) do |session|
   @result = session.exec!('cd ems
     ruby sim_simple.rb')
   puts @result
-end
-
-puts 'Now ssh-ing into epc'
-
-Net::SSH.start(h2, u) do |session|
-  session.exec!('git clone https://github.com/HunterMoffat/ems.git')
-  session.exec!('cd #ems')
-  session.exec!('ruby epc.rb')
 end
--- sim_simple.rb
+++ sim_simple.rb
@@ -1,15 +1,12 @@
 # This hopefully sets up the sim-ran node
-command = Thread.new do 
-  system("cd ..
-    sudo apt-get remove -y --purge man-db
-    sudo bash
-    cd /opt
-    sudo systemctl start mongodb
-    git clone https://gitlab.flux.utah.edu/jczhu/oaisim-xran/
-    cd oaisim-xran
-    git checkout develop
-    cd cmake_targets
-    sudo ./build_oai -I -c -C --eNB --UE -w SIMU
-    ")
-end
-command.join
+system("cd ..
+  sudo apt-get remove -y --purge man-db
+  sudo bash
+  cd /opt
+  git clone https://gitlab.flux.utah.edu/jczhu/oaisim-xran/
+  cd oaisim-xran
+  git checkout develop
+  cd cmake_targets
+  sudo ./build_oai -I -c -C --eNB --UE -w SIMU
+  ")
+
