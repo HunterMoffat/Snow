3336024c6cdfa8b6e7b29cb8d673a3e0ba78ab9a
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-08-04 21:56:03 -0600

    Add files via upload

--- Scripts/epc_simple.rb
+++ Scripts/epc_simple.rb
@@ -9,23 +9,20 @@ system("sudo apt-get remove -y --purge man-db
         libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev
         sudo apt-get -y install nodejs
         
-        sudo bash
         sudo cd /opt
         sudo git clone https://github.com/nextepc/nextepc
         sudo cd nextepc
         sudo autoreconf -iv
-        ./configure --prefix=`pwd`/install
+        sudo ./configure --prefix=`pwd`/install
         sudo make -j `nproc`
         sudo make install
-        exit
-        
-        sudo bash
+
         cat << EOF > /etc/systemd/network/98-nextepc.netdev
         [NetDev]
         Name=pgwtun
         Kind=tun
         EOF
-        exit
+
         sudo systemctl restart systemd-networkd
         sudo ip addr add 192.168.0.1/24 dev pgwtun
         sudo ip link set up dev pgwtun
