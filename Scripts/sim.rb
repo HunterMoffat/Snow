9a3d841018739d17bb918781366d5eecfeb1b5a7
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-08-04 22:47:03 -0600

    Add files via upload

--- Scripts/sim.rb
+++ Scripts/sim.rb
@@ -1,8 +1,7 @@
 system("sudo apt-get remove -y --purge man-db
-        sudo bash &&
-        cd /opt &&
-        git clone https://gitlab.flux.utah.edu/jczhu/oaisim-xran/ &&
-        cd oaisim-xran &&
-        git checkout develop &&
-        cd cmake_targets &&
+        cd /opt
+        sudo git clone https://gitlab.flux.utah.edu/jczhu/oaisim-xran/
+        cd oaisim-xran
+        git checkout develop
+        cd cmake_targets
         echo yes | sudo ./build_oai -I -c -C --eNB --UE -w SIMU")
