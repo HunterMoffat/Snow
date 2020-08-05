adab33818798ef8de5e0fe545a7b06abd95950e6
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-08-05 13:39:43 -0600

    Add files via upload

--- Scripts/epc1.rb
+++ Scripts/epc1.rb
@@ -23,11 +23,4 @@ system("sudo apt-get remove -y --purge man-db
         sudo ./configure --prefix=`pwd`/install
         sudo make -j `nproc`
         sudo make install
-
-        sudo systemctl restart systemd-networkd
-        sudo ip addr add 192.168.0.1/24 dev pgwtun
-        sudo ip link set up dev pgwtun
-        sudo iptables -t nat -A POSTROUTING -o `cat /var/emulab/boot/controlif` -j MASQUERADE
-        cd /opt/nextepc/install/etc/nextepc
-        cp /proj/reu2020/reudata/nextepc.conf   
         ")
