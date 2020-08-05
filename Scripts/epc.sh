dee954a8bf540c0da866b62ebabca3f157f41f4c
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-08-04 18:24:18 -0600

    Add files via upload

--- Scripts/epc.sh
+++ Scripts/epc.sh
@@ -18,19 +18,20 @@ autoreconf -iv
 make -j `nproc`
 make install
 
-cd /opt/nextepc/webui
-sudo npm install
+# cd /opt/nextepc/webui
+# sudo npm install
 
-cat << EOF > /etc/systemd/network/98-nextepc.netdev
-[NetDev]
-Name=pgwtun
-Kind=tun
-EOF
+# cat << EOF > /etc/systemd/network/98-nextepc.netdev
+# [NetDev]
+# Name=pgwtun
+# Kind=tun
+# EOF
 
-sudo systemctl restart systemd-networkd
-sudo ip addr add 192.168.0.1/24 dev pgwtun
-sudo ip link set up dev pgwtun
-sudo iptables -t nat -A POSTROUTING -o `cat /var/emulab/boot/controlif` -j MASQUERADE
-cd /opt/nextepc/install/etc/nextepc
-cp /proj/reu2020/reudata/nextepc.conf
-echo DONE
+# sudo systemctl restart systemd-networkd
+# sudo ip addr add 192.168.0.1/24 dev pgwtun
+# sudo ip link set up dev pgwtun
+# sudo iptables -t nat -A POSTROUTING -o `cat /var/emulab/boot/controlif` -j MASQUERADE
+# cd /opt/nextepc/install/etc/nextepc
+# cp /proj/reu2020/reudata/nextepc.conf
+# echo DONE
+return 0
