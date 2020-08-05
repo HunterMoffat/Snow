9ad1797129f53ce4ef4848963ec394594f51dce4
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-08-04 22:17:52 -0600

    Add files via upload

---
+++ Scripts/epc2.rb
@@ -0,0 +1,8 @@
+
+system("sudo systemctl restart systemd-networkd
+        sudo ip addr add 192.168.0.1/24 dev pgwtun
+        sudo ip link set up dev pgwtun
+        sudo iptables -t nat -A POSTROUTING -o `cat /var/emulab/boot/controlif` -j MASQUERADE
+        cd /opt/nextepc/install/etc/nextepc
+        cp /proj/reu2020/reudata/nextepc.conf   
+        ")
---
+++ Scripts/sim.rb
@@ -0,0 +1,7 @@
+system("sudo apt-get remove -y --purge man-db
+        cd /opt
+        git clone https://gitlab.flux.utah.edu/jczhu/oaisim-xran/
+        cd oaisim-xran
+        git checkout develop
+        cd cmake_targets
+        echo yes | sudo ./build_oai -I -c -C --eNB --UE -w SIMU")
