ca808f76de1c0d9fc76616e34ec64f94549eb6ae
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-08-04 20:54:14 -0600

    Add files via upload

--- Scripts/epc2.sh
+++ Scripts/epc2.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-sudo /opt/nextepc/install/bin/nextepc-epcd
-exit
+sudo bash
+
+cd /opt
+git clone https://github.com/nextepc/nextepc
+cd nextepc
+autoreconf -iv
+./configure --prefix=`pwd`/install
+make -j `nproc`
+make install
+echo exit
+
+cd /opt/nextepc/webui
+sudo npm install
