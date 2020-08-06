b42aca526f9707f5f95ae905e707a49df60f14cd
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-08-06 08:59:34 -0600

    Add files via upload

--- Scripts/epc2.rb
+++ Scripts/epc2.rb
@@ -1,7 +1,7 @@
 require 'fileutils'
-file = File.open("98-nextepc.netdev", 'w+')
+file = File.open("/etc/systemd/network/98-nextepc.netdev", 'w+')
 File.write(file, "[NetDev]
                 Name=pgwtun
                 Kind=tun
                 ")
-system("cd Snow/Scripts && sudo cp 98-nextepc.netdev ~/etc/systemd/network/")
+# system("cd Snow/Scripts && sudo cp 98-nextepc.netdev /etc/systemd/network/")
