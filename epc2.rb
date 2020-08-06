6ef1ea283f18dc05eefd5bd37c63426da0d15d2b
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-08-06 08:51:58 -0600

    Delete epc2.rb

--- epc2.rb
+++
@@ -1,7 +0,0 @@
-require 'fileutils'
-file = File.open("98-nextepc.netdev", 'w+')
-File.write(file, "[NetDev]
-                Name=pgwtun
-                Kind=tun
-                ")
-system("cd Snow/Scripts && sudo cp 98-nextepc.netdev ~/opt/systemd/network/")
