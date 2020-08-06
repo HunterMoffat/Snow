9ccbd394aa22abd88138f7ba8899862f5febef51
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-08-06 07:54:21 -0600

    Add files via upload

--- Scripts/epc1.rb
+++ Scripts/epc1.rb
@@ -1,6 +1,6 @@
 # A simple script that will auto set up the sim ran node for the first tutorial
 # installing mongo db
-require 'fileutils'
+
 
 # cat << EOF > /etc/systemd/network/98-nextepc.netdev
 #         
@@ -14,18 +14,15 @@ system("sudo systemctl start mongodb")
 system("echo yes | sudo apt-get -y install autoconf libtool gcc pkg-config \
         git flex bison libsctp-dev libgnutls28-dev libgcrypt-dev \
         libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev")
-puts "DONE WITH THE FIRST"
+puts "[DONE WITH THE FIRST]"
 system("sudo apt-get -y install nodejs")
 system("cd /opt && sudo git clone https://github.com/nextepc/nextepc")
 system("cd /opt/nextepc && sudo autoreconf -iv && 
         sudo ./configure --prefix=`pwd`/install 
         && sudo make -j `nproc` && 
         sudo make install")
-puts "ABOUT TO WRITE THE FILE"
-file = File.open("/opt/nextepc/install/98-nextepc.netdev")
-File.write(file, "[NetDev]
-                Name=pgwtun
-                Kind=tun")
+puts "[ABOUT TO WRITE THE FILE]"
+system("cd Snow/Scripts/ sudo ruby epc2.rb")
 system("sudo systemctl restart systemd-networkd")
 system("sudo ip addr add 192.168.0.1/24 dev pgwtun")
 system("sudo ip link set up dev pgwtun")
--- Scripts/epc2.rb
+++ Scripts/epc2.rb
@@ -1,7 +1,6 @@
-system("sudo systemctl restart systemd-networkd
-        sudo ip addr add 192.168.0.1/24 dev pgwtun
-        sudo ip link set up dev pgwtun
-        sudo iptables -t nat -A POSTROUTING -o `cat /var/emulab/boot/controlif` -j MASQUERADE
-        cd /opt/nextepc/install/etc/nextepc
-        cp /proj/reu2020/reudata/nextepc.conf   
-        ")
+require 'fileutils'
+file = File.open("/opt/nextepc/install/98-nextepc.netdev")
+File.write(file, "[NetDev]
+                Name=pgwtun
+                Kind=tun
+                EOF")
