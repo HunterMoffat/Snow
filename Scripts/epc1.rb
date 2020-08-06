7d8421b6ff624c7e8a401a5d5b4c94f9d3eb9e30
HunterMoffat <42703453+HunterMoffat@users.noreply.github.com>
2020-08-06 08:12:29 -0600

    Add files via upload

--- Scripts/epc1.rb
+++ Scripts/epc1.rb
@@ -1,7 +1,7 @@
 # A simple script that will auto set up the sim ran node for the first tutorial
 # installing mongo db
 
-
+require 'fileutils'
 # cat << EOF > /etc/systemd/network/98-nextepc.netdev
 #         
 #         Name=pgwtun
@@ -14,15 +14,18 @@ system("sudo systemctl start mongodb")
 system("echo yes | sudo apt-get -y install autoconf libtool gcc pkg-config \
         git flex bison libsctp-dev libgnutls28-dev libgcrypt-dev \
         libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev")
-puts "[DONE WITH THE FIRST]"
+puts "\n[DONE WITH THE FIRST]\n"
 system("sudo apt-get -y install nodejs")
 system("cd /opt && sudo git clone https://github.com/nextepc/nextepc")
-system("cd /opt/nextepc && sudo autoreconf -iv && 
-        sudo ./configure --prefix=`pwd`/install 
-        && sudo make -j `nproc` && 
-        sudo make install")
-puts "[ABOUT TO WRITE THE FILE]"
-system("cd Snow/Scripts/ sudo ruby epc2.rb")
+system("cd /opt/nextepc && sudo autoreconf -iv && sudo ./configure --prefix=`pwd`/install && sudo make -j `nproc` && sudo make install")
+puts "\n[ABOUT TO WRITE THE FILE]\n"
+# system("cd Snow/Scripts/ sudo ruby epc2.rb")
+file = File.open("/opt/nextepc/install/98-nextepc.netdev")
+File.write(file, "[NetDev]
+                Name=pgwtun
+                Kind=tun
+                EOF")
+puts "\n[DONE WRITING FILE #{file}]\n"
 system("sudo systemctl restart systemd-networkd")
 system("sudo ip addr add 192.168.0.1/24 dev pgwtun")
 system("sudo ip link set up dev pgwtun")
